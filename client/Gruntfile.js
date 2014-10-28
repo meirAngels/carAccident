770e1d39d0658db27be94223ac5d22ee274e18e4
Meir Rotstein <meir.rotstein@sap.com>
2014-10-28 10:21:32 +0200

    project kickoff

---
+++ client/Gruntfile.js
@@ -0,0 +1,151 @@
+//Gruntfile
+module.exports = function(grunt) {
+
+    var SRC_BASE = './app';
+    var DST_BASE = './public';
+    var LIVERELOAD_PORT = 8989;
+
+	//Initializing the configuration object
+    grunt.initConfig( {});
+
+	// Plugin loading
+    grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-express');
+
+
+    // Environment settings
+    grunt.config('pkg', grunt.file.readJSON('package.json'));
+    grunt.config('env',      grunt.option('env') || process.env.GRUNT_ENV || 'dev');
+    grunt.config('compress', grunt.config('env') === 'prod' || grunt.config('env') === 'production');
+
+    // Task configuration
+    grunt.config('clean', [DST_BASE]);
+    grunt.config('copy', {
+                    main: {
+                        src: SRC_BASE + '/main/index.html',
+                        dest: DST_BASE + '/index.html'
+                    },
+                    glyphicons: {
+                        cwd: 'bower_components/bootstrap/dist/fonts/',
+                        src: '*',
+                        dest: DST_BASE + '/fonts/',
+                        flatten: true,
+                        expand: true
+                    },
+                    bootstrap: {
+                        cwd: 'bower_components/bootstrap/dist/',
+                        src: '**/*',
+                        dest: DST_BASE + '/assets/bootstrap/',
+                        flatten: false,
+                        expand: true
+                    },
+                    font_awesome: {
+                        cwd: 'bower_components/font-awesome/fonts/',
+                        src: '*',
+                        dest: DST_BASE + '/fonts/',
+                        flatten: true,
+                        expand: true
+                    },
+                    images: {
+                        cwd: SRC_BASE + '/assets/img/',
+                        src: '*',
+                        dest: DST_BASE + '/assets/img/',
+                        flatten: true,
+                        expand: true
+                    }
+
+                });
+    grunt.config('concat', {
+                    options: {
+                        separator: ';'
+                    },
+                    js_frontend: {
+                        src: [
+                                './bower_components/jquery/dist/jquery.js',
+                                './bower_components/bootstrap-rtl/dist/js/bootstrap.js',
+                                './bower_components/typeahead.js/dist/typeahead.bundle.js',
+
+                                SRC_BASE + '/main/frontend.main.js'
+                        ],
+                        dest: DST_BASE + '/javascript/frontend.js'
+                    }
+                });
+    grunt.config('less', {
+                    development: {
+                        options: {
+                            compress: grunt.config('compress')
+                        },
+                        files: {
+                            './public/styles/frontend.css': ['./app/main/frontend.main.less'],
+                            './public/assets/fonts/font-awesome.css': ['./bower_components/font-awesome/less/font-awesome.less']
+                        }
+                    }
+                });
+    grunt.config('uglify', {
+                    options: {
+                        mangle: false	// Use if you want the names of your functions and variables unchanged
+                    },
+                    frontend: {
+                        files: [{
+                            src:  DST_BASE + '/javascript/frontend.js',
+                            dest: DST_BASE + '/javascript/frontend.js'
+                        }]
+                    }
+                });
+
+    grunt.config('express', {
+                    all: {
+                        options: {
+                            bases: ['./public'],
+                            port: LIVERELOAD_PORT,
+                            hostname: '0.0.0.0',
+                            livereload: true
+                        }
+                    },
+                    stylish: {
+                        options: {
+                            bases: ['./tmp/stylish-portfolio'],
+                            port: 9898,
+                            hostname: '0.0.0.0'
+                        }
+                    }
+                });
+
+    grunt.config('watch', {
+                    options: {
+                        nospawn: true
+                    },
+                    less: {
+                        files: ['app/**/*.less'],
+                        tasks: ['less']
+                    },
+                    all: {
+                        options: {
+                            livereload: true
+                        },
+                        files: [
+                            'app/**/*.less',
+                            'app/**/*.html',
+                            'app/**/*.css',
+                            'app/**/*.js',
+                            'app/**/*.{png,jpg,jpeg,gif,webp,svg}'
+                        ],
+                        tasks: ['build']
+                    }
+                });
+
+	// Task definition
+	grunt.registerTask('default', ['build', 'express', 'watch']);
+    var build_tasks = ['less', 'concat', 'copy'];
+    if (grunt.config('compress')) {
+        build_tasks.push('uglify');
+    }
+    grunt.registerTask('build', build_tasks);
+    grunt.registerTask('full', ['clean', 'build']);
+
+};
---
+++ client/README.md
@@ -0,0 +1,24 @@
+Build
+=====
+
+    npm install
+    bower install
+
+
+Angular conventions
+===================
+* Create a folder for a module
+* State the file type in it's name (JS only)
+* use lower case with dashes between words
+
+example:
+
+
+    - app
+      - my-module
+        - my-service.srv.js
+        - my-contoller.srv.js
+        - my-directive.drv.js
+        - my-filter.fltr.js
+        - my-style.less
+        - my-markup.html
