751a8279849815fe9acf61adc939fa502d71cb6d
Tsahar Sobe <tsahar.sobe@sap.com>
2014-11-09 11:12:56 +0200

    some changes

--- server/src/com/sap/carAccident/servlets/Operation.java
+++
@@ -1,9 +0,0 @@
-package com.sap.carAccident.servlets;
-
-public enum Operation {
-	   SET_TOWING_ETA, 
-	   SET_CAR_REPLACEMENT_ETA, 
-	   SET_CLAIM_SENT, 
-	   SET_CLAIM_STATUS, 
-	   CREATE;   	  
-}
--- server/src/com/sap/caraccident/persistence/ClaimStatus.java
+++
@@ -1,3 +0,0 @@
-package com.sap.carAccident.persistence;
-
-public enum ClaimStatus {OPEN, IN_PROCESS, CLOSED}
